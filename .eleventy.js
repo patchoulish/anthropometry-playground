9a1277f30585d6ef1318a9302a4b648093da6bc5
patchoulish <self@patchouli.sh>
2025-12-30 16:20:55 -0500

    Add logic to convert ANSUR II data to JSON

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 import path from "node:path";
 import * as sass from "sass";
+import csvToJson from "convert-csv-to-json";
 
 export default async function (eleventyConfig) {
 	// Output directory: _site
@@ -38,14 +39,59 @@ export default async function (eleventyConfig) {
 		},
 	});
 
+	// Add csv-to-json support.
+	eleventyConfig.addExtension("csv", {
+		outputFileExtension: "json",
+
+		// Opt-out of eleventy's layouts.
+		useLayouts: false,
+
+		compile: async function (inputContent, inputPath) {
+			let parsedPath = path.parse(inputPath);
+
+			let result = csvToJson
+				.fieldDelimiter(",")
+				.trimHeaderFieldWhiteSpace(true)
+				.getJsonFromCsv(inputPath);
+
+			let columnarResult = {};
+			for (let row of result) {
+				for (let [key, value] of Object.entries(row)) {
+					if (!(key in columnarResult)) {
+						columnarResult[key] = [];
+					}
+					// Convert numeric values from strings to numbers.
+					let numericValue = Number(value);
+					if (!isNaN(numericValue)) {
+						columnarResult[key].push(numericValue);
+					} else {
+						columnarResult[key].push(value);
+					}
+				}
+			}
+
+			result = columnarResult;
+
+			return async (data) => {
+				return JSON.stringify(result);
+			};
+		},
+	});
+
 	// Ignore GitHub-related configuration and content files.
 	eleventyConfig.ignores.add(".github/**");
 
 	// Register "scss" as a template format.
 	eleventyConfig.addTemplateFormats("scss");
+	// Register "csv" as a template format.
+	eleventyConfig.addTemplateFormats("csv");
 
 	// Passthrough copy for static assets.
-	eleventyConfig.addPassthroughCopy("assets/scripts/");
+	eleventyConfig.addPassthroughCopy({
+		"assets/scripts/": "assets/scripts/",
+		"assets/data/ansur2/measurements.json":
+			"assets/data/ansur2/measurements.json",
+	});
 
 	eleventyConfig.addPassthroughCopy({
 		"node_modules/choices.js/public/assets/scripts/choices.min.js":
