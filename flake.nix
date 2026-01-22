4a6ea7148cd88930950768cd292db112cf0ad9d3
patchoulish <self@patchouli.sh>
2026-01-22 16:46:35 +0000

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -65,20 +65,20 @@
           packages = requiredPackages;
 
           shellHook = ''
-            
-                        echo "Setting up Git hooks..."
-                        if [ -f .git/hooks/pre-commit ]; then
-                          echo "Git hooks already installed."
-                        else
-                          pre-commit install
-                          echo "Git hooks installed."
-                        fi
-                        echo "Updating Node dependencies..."
-                        npm install
-                        echo "Node dependencies updated."
-                        echo ""
-                        echo "Welcome to the anthropometry-playground development shell."
-                        echo "Run 'just' to see available recipes."
+
+            echo "Setting up Git hooks..."
+            if [ -f .git/hooks/pre-commit ]; then
+              echo "Git hooks already installed."
+            else
+              pre-commit install
+              echo "Git hooks installed."
+            fi
+            echo "Updating Node dependencies..."
+            npm install
+            echo "Node dependencies updated."
+            echo ""
+            echo "Welcome to the anthropometry-playground development shell."
+            echo "Run 'just' to see available recipes."
           '';
         };
       }
